764fc95d424efd0741358ea0965fb24888743fcd
Shubhamdhanola <126813134+Shubhamdhanola@users.noreply.github.com>
2023-05-14 22:56:12 +0530

    Add files via upload

---
+++ car_repair.php
@@ -0,0 +1,391 @@
+<html>
+<head>
+<title>www.car repair.com</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/456286dfa8.js" crossorigin="anonymous"></script>
+
+  
+<style>
+	body{
+		font-family:lato;
+	}
+	.navbar{
+		background:white;
+		margin-bottom:0px
+	}
+	.navbar li{
+		font-size:30px;
+	}
+	.navbar li a{
+		padding:8px;
+	}
+	.navbar li:first-child{
+		font-size:15px;
+	}
+	.navbar-header img{
+		height:62px;
+	}
+	.brand{
+		padding-left:10px;
+	}
+	.brand h2{
+		font-weight:bold;
+		font-size:20px;
+	}
+	.title_img img{
+		width:100%;
+		height:550px;
+	}
+	.featured .thumbnail{
+		border:2px solid #397dc0;
+		padding:30 20 30 20px;
+		width:220px;
+	}
+	.featured .glyphicon{
+		font-size:30px;
+		color:#397dc0;
+	}
+	.featured h4{
+		padding-top:10px;
+		padding-bottom:10px;
+		color:#397dc0;
+		font-weight:bold;
+	}
+	.demo .btn{
+		background:black;
+		color:white;
+		border-radius:2px;
+		border:1px solid white;
+		font-size:12px;
+	}
+	.testimonials{
+		position:relative;
+	}
+	.testimonials h2{
+		position:relative;
+		font-size:40px;
+		letter-spacing:2px;
+	}
+	.carousel-inner{
+        height:300px;
+        background:white;
+    }
+    .carousel-inner p{
+       padding-top:50px;
+    }
+	.symbol{
+		font-size:50px;
+		color:grey;
+	}
+	.symbol_btm{
+		font-size:50px;
+		color:grey;
+		position:absolute;
+		bottom:100px;
+		right:0px;
+	}
+	.carousel-inner img{
+        height:65px;
+		width:65px;
+		margin:auto;
+    }
+	footer{
+		background:#397dc0;
+		padding:10px;
+	}
+	footer p{
+		color:white;
+		padding-top:10px;
+		display:flex;
+		justify-content:end;
+	}
+	footer li a{
+		font-weight:bold;
+	}
+	
+	@media only screen and (min-width:240px)and (max-width:760px)
+	{
+		.navbar li{
+			font-size:20px;
+		}
+		.navbar li a{
+			padding:8px;
+		}
+		.navbar li:first-child{
+			font-size:15px;
+		}
+		.navbar-header img{
+			height:62px;
+		}
+		.brand{
+			padding-left:5;
+		}
+		
+		.featured .thumbnail{
+			border:2px solid #397dc0;
+			padding:auto;
+			margin:auto;
+			margin-bottom:10px;
+			width:220px;
+		}
+		.featured .glyphicon{
+			font-size:30px;
+			color:#397dc0;
+		}
+		.featured h4{
+			padding-top:10px;
+			padding-bottom:10px;
+			color:#397dc0;
+			font-weight:bold;
+		}
+		.testimonials h2{
+		font-size:40px;
+		letter-spacing:2px;
+		}
+		.carousel-inner{
+			height:auto;
+			background:white;
+		}
+		.carousel-inner p{
+		   padding-top:50px;
+		}
+		.symbol{
+			font-size:0px;
+			color:grey;
+			margin:0px;
+		}
+		.symbol_btm{
+			font-size:0px;
+			color:grey;
+			margin:0px;
+		}
+		.carousel-inner img{
+			height:65px;
+			width:65px;
+			margin:auto;
+		}
+		footer p{
+		color:white;
+		font-size:12px;
+		padding-top:10px;
+		display:flex;
+		justify-content:center;
+		}
+	}
+	
+	@media only screen and (min-width:761px)and (max-width:1200px)
+	{
+		.featured .thumbnail{
+			border:2px solid #397dc0;
+			padding:auto;
+			margin-bottom:30px;
+			width:auto;
+		}
+		.featured .glyphicon{
+			font-size:30px;
+			color:#397dc0;
+		}
+		.featured h4{
+			padding-top:10px;
+			padding-bottom:10px;
+			color:#397dc0;
+			font-weight:bold;
+		}
+		.carousel-inner{
+			height:auto;
+			background:white;
+		}
+		.carousel-inner img{
+			height:65px;
+			width:65px;
+			margin:auto;
+		}
+		
+	}
+</style>
+
+<body>
+
+	<nav class="navbar">
+	  <div class="container">
+		<br>
+			<div class="navbar-header">
+			  <img src="images/car_logo.png">
+			</div>
+			<div class="navbar-header brand ">
+				<h2 style="color:#1e5a95"> CARS <span style="color:black"> REPAIR</span></h2>
+			</div>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="" style="color:#397dc0">Follow us on:</a></li>
+			<li><a href=""><i class="fa-brands fa-square-twitter" style="color: #636363;"></i></a></li>
+			<li><a href=""><i class="fa-brands fa-facebook" style="color: #626060;"></i></a></li>
+			<li><a href=""><i class="fa-brands fa-google-plus" style="color: #626364;"></i></a></li>
+		</ul>
+	  </div>
+	  <br>
+	</nav>
+
+<!--Container (Start Repairing)-->
+<div class="container-fluid title_img">
+		<img src="https://ak8.picdn.net/shutterstock/videos/33875458/thumb/6.jpg " class="img-responsive">
+</div>
+
+<!-- Container(feature Repairing)-->
+<div class="container-fluid featured" style="background:lightgrey;">
+	<div class="container text-center">
+	<br>
+		<h2><b>FEATURED  REPAIRING SERVICES</b></h2>
+		<br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>Aempor incididunt ut labore et dolore magna aliqua.</p>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="thumbnail">
+				<p><span class="glyphicon glyphicon-calendar"></span></p>
+				<h4>Fast Low Cost Frieght</h4>
+				<div class="caption">				
+				<p>Lorizzle ipsizzle dolor sizzle amizzle, ma nizzle adipiscing pot. Izzle sapien velit, fizz gangsta.</p>
+				</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-3">
+				<div class="thumbnail">
+				<p><span class="glyphicon glyphicon-calendar"></span></p>
+				<h4>Fast Low Cost Frieght</h4>
+				<div class="caption">
+				<p>Lorizzle ipsizzle dolor sizzle amizzle, ma nizzle adipiscing pot. Izzle sapien velit, fizz gangsta.</p>
+				</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-3">
+				<div class="thumbnail">
+				<p><span class="glyphicon glyphicon-calendar"></span></p>
+				<h4>Fast Low Cost Frieght</h4>
+				<div class="caption">
+				<p>Lorizzle ipsizzle dolor sizzle amizzle, ma nizzle adipiscing pot. Izzle sapien velit, fizz gangsta.</p>
+				</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-3">
+				<div class="thumbnail">
+				<p><span class="glyphicon glyphicon-calendar"></span></p>
+				<h4>Fast Low Cost Frieght</h4>
+				<div class="caption">				
+				<p>Lorizzle ipsizzle dolor sizzle amizzle, ma nizzle adipiscing pot. Izzle sapien velit, fizz gangsta.</p>
+				</div>
+				</div>
+			</div>
+		</div>		
+	</div>
+</div>
+
+<!-- Container( Demo video)-->
+<div class="container-fluid demo" style="background:black; color:white;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<br>
+				<br>
+				<h2> SEE DEMO VIDEO </h2>
+				<br>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<br>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+				<br>
+				<br>
+				<p><button class="btn btn-default btn-lg">SEE VIDEO</button></p>
+				<br>
+				<br>
+				<br>
+			</div>
+	
+			<div class="col-sm-6">
+			<br>
+			<br>
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/H2BF54GUzuk"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Container (Testimonials)-->
+<div class="container-fluid text-center testimonials">
+	<br>
+    <br>
+	<h2><strong>TESTIMONIALS</strong></h2>
+  
+	<div class="row">
+		<div class="col-sm-2 col-md-2 symbol">
+			<p>"</p>
+		</div>
+	  
+		<div class="col-sm-8 col-md-8">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				
+					<!-- Wrapper for slides -->
+				<div class="carousel-inner text-center">
+						<div class="item active">
+								<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>Lorem ipsum dolor sit amet consectetur. </p>
+								<br>
+								<h5 style="color:#397dc0;">Shubham Dhanola<span style="color:grey;"><i>,Director</i></span></h5>
+								<br>
+								<img src="https://clipground.com/images/img_avatar2-clipart-8.png" class="img-circle img-responsive" >
+						</div>
+
+						<div class="item">
+								<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>Lorem ipsum dolor sit amet consectetur. </p>
+								<br>
+								<h5 style="color:#397dc0;">Aman Singh<span style="color:grey;"><i>,Manager</i></span></h5>
+								<br>
+								<img src="https://clipground.com/images/img_avatar2-clipart-8.png" class="img-circle img-responsive" >
+							</div>
+						
+							<div class="item">
+								<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>Lorem ipsum dolor sit amet consectetur. </p>
+								<br>
+								<h5 style="color:#397dc0;">Siddharth<span style="color:grey;"><i>,Advisor</i></span></h5>
+								<br>
+								<img src="https://clipground.com/images/img_avatar2-clipart-8.png" class="img-circle img-responsive" >
+						</div>
+					<br>
+				</div>
+			</div>
+		</div>
+		  
+		<div class="col-sm-2 col-md-2 symbol_btm">
+				<p>"</p>
+		</div>
+	</div>
+</div>
+</body>
+
+<!--Footer-->
+<footer>
+	<div class="container">
+		<ul class="nav navbar-nav navbar-left">
+			<li><a href="" style="color:white">Follow us on:</a></li>
+			<li><a href=""><i class="fa-brands fa-square-twitter" style="color: white;"></i></a></li>
+			<li><a href=""><i class="fa-brands fa-facebook" style="color: white;"></i></a></li>
+			<li><a href=""><i class="fa-brands fa-google-plus" style="color: white;"></i></a></li>
+		</ul>
+		<p> Copyright @ 2023.A Car repair landing Page by Shubham</p>
+	</div>	
+</footer>
+
+</body>
+</html>
